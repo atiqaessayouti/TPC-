e6d37a4d84ab799a9a95413c6e2c2bddde092118
atiqaessayouti <atiqaessayouti@gmail.com>
2025-03-12 14:30:07 +0100

    Update Ex5.cpp

--- TpC/Ex5.cpp
+++ TpC/Ex5.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <vector>
 #include <limits>
 
 int main() {
-    std::vector<std::vector<int>> tableau = {
+    const int rows = 3;
+    const int cols = 4;
+    int tableau[rows][cols] = {
         {-2, 1, -3, 4},
         {-1, 2, 1, -5},
         {4, -1, 2, 1}
@@ -13,10 +14,10 @@ int main() {
     int debutLigne = 0, finLigne = 0;
     int debutCol = 0, finCol = 0;
 
-    for (int i = 0; i < tableau.size(); i++) {
-        for (int j = 0; j < tableau[0].size(); j++) {
-            for (int k = i; k < tableau.size(); k++) {
-                for (int l = j; l < tableau[0].size(); l++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            for (int k = i; k < rows; k++) {
+                for (int l = j; l < cols; l++) {
                     int sommeActuelle = 0;
 
                     for (int m = i; m <= k; m++) {
@@ -48,4 +49,3 @@ int main() {
 
     return 0;
 }
-
